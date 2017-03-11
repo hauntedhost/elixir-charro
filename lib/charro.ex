70434fb833a846b334e8a5ee1566ae957833501a
Sean Omlor <somlor@eml.cc>
2017-03-11 00:32:00 -0500

    new mix otp app

---
+++ lib/charro.ex
@@ -0,0 +1,18 @@
+defmodule Charro do
+  @moduledoc """
+  Documentation for Charro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Charro.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/charro_test.exs
@@ -0,0 +1,8 @@
+defmodule CharroTest do
+  use ExUnit.Case
+  doctest Charro
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
